e899664afe257ee0da02b329fb7550a306f7b9e0
Marcus Ahnve <marcus@ahnve.com>
2015-11-18 23:34:49 +0100

    Clj + python

---
+++ arithmetic/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to arithmetic
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ python/src/arithmetic.py
@@ -0,0 +1,2 @@
+def cumulative_interest(initial_amount, interest, years):
+    return initial_amount * ((1 + interest) ** years)
